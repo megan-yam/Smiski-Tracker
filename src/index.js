4f957c23975debf2c14132ba1f06a63b10d5d461
Megan Yam <119703973+megan-yam@users.noreply.github.com>
2024-08-26 21:47:34 -0700

    Fully removed old found feature

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom";
 import "./index.css";
 import App from "./App";
-import { FoundProvider } from "./Found";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <FoundProvider>
-      <App />
-    </FoundProvider>
+    <App />
   </React.StrictMode>
 );
