9f9b2309003e2471b99a6cced35145fcf6d0ba6f
Megan Yam <119703973+megan-yam@users.noreply.github.com>
2024-08-26 21:46:52 -0700

    Reimplement Found feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-import Navbar from "./NavBar";
 import Series1 from "./Series1";
 
 function App() {
+  const [found, setFound] = useState([]);
+  const [wishlist, setWish] = useState([]);
+
+  function addFound(discovered) {
+    if (!found.includes(discovered)) {
+      const newFound = [...found, discovered];
+      setFound(newFound);
+    }
+  }
+
+  function removeFound(item) {
+    setFound(found.filter((i) => i !== item));
+  }
+
+  function addToList(wish) {
+    const newList = [...wishlist, wish];
+    setWish(newList);
+  }
+
+  function removeFromList(index) {
+    const newList = wishlist.filter((wishlist, listIndex) => {
+      return listIndex !== index;
+    });
+    setFound(newList);
+  }
+
   return (
     <>
-      <div>
-        <Navbar />
-        <Series1 />
+      <div className="landing">
+        {/* <Navbar /> */}
+        <Series1 found={found} addFound={addFound} removeFound={removeFound} />
       </div>
     </>
   );
--- src/Series1.js
+++ src/Series1.js
@@ -7,93 +7,100 @@ import lounging from "./imgs/series1/lounging.png";
 import hiding from "./imgs/series1/hiding.png";
 import peeking from "./imgs/series1/peeking.png";
 
-function Series1() {
-  const [clickedImageIds, setClickedImageIds] = useState([]);
+function Series1(props) {
+  const { addFound, removeFound, found } = props;
 
-  const handleClick = (id) => {
-    if (clickedImageIds.includes(id)) {
-      setClickedImageIds(
-        clickedImageIds.filter((clickedId) => clickedId !== id)
-      );
+  const handleClick = (item) => {
+    if (found.includes(item)) {
+      removeFound(item);
     } else {
-      setClickedImageIds([...clickedImageIds, id]);
+      addFound(item);
     }
   };
-
   return (
     <>
-      <h1>Series 1</h1>
-      <div className="hugging-knees">
-        <img
-          src={hugging}
-          alt="Hugging Knees"
-          style={{
-            filter: clickedImageIds.includes("hugging")
-              ? "brightness(100%)"
-              : "brightness(50%)",
-          }}
-          onClick={() => handleClick("hugging")}
-        />
-      </div>
-      <div className="sitting">
-        <img
-          src={sitting}
-          alt="Sitting"
-          style={{
-            filter: clickedImageIds.includes("sitting")
-              ? "brightness(100%)"
-              : "brightness(50%)",
-          }}
-          onClick={() => handleClick("sitting")}
-        />
-      </div>
-      <div className="looking-back">
-        <img
-          src={lookingBack}
-          alt="Looking Back"
-          style={{
-            filter: clickedImageIds.includes("looking-back")
-              ? "brightness(100%)"
-              : "brightness(50%)",
-          }}
-          onClick={() => handleClick("looking-back")}
-        />
-      </div>
-      <div className="lounging">
-        <img
-          src={lounging}
-          alt="Lounging"
-          style={{
-            filter: clickedImageIds.includes("lounging")
-              ? "brightness(100%)"
-              : "brightness(50%)",
-          }}
-          onClick={() => handleClick("lounging")}
-        />
-      </div>
-      <div className="hiding">
-        <img
-          src={hiding}
-          alt="Hiding"
-          style={{
-            filter: clickedImageIds.includes("hiding")
-              ? "brightness(100%)"
-              : "brightness(50%)",
-          }}
-          onClick={() => handleClick("hiding")}
-        />
-      </div>
-      <div className="peeking">
-        <img
-          src={peeking}
-          alt="Peeking"
-          style={{
-            filter: clickedImageIds.includes("peeking")
-              ? "brightness(100%)"
-              : "brightness(50%)",
-          }}
-          onClick={() => handleClick("peeking")}
-        />
+      <div className="series1">
+        <a href="https://smiski.com/e/products/series-1/">Series 1</a>
+        <div className="hugging-knees">
+          <img
+            src={hugging}
+            alt="Hugging Knees"
+            className="series-img"
+            style={{
+              filter: found.includes("hugging")
+                ? "brightness(100%)"
+                : "brightness(50%)",
+            }}
+            onClick={() => handleClick("hugging")}
+          />
+          <i class="fa-regular fa-heart"></i>
+        </div>
+        <div className="sitting">
+          <img
+            src={sitting}
+            alt="Sitting"
+            style={{
+              filter: found.includes("sitting")
+                ? "brightness(100%)"
+                : "brightness(50%)",
+            }}
+            addFound
+            onClick={() => handleClick("sitting")}
+          />
+          <i class="fa-regular fa-heart"></i>
+        </div>
+        <div className="looking-back">
+          <img
+            src={lookingBack}
+            alt="Looking Back"
+            style={{
+              filter: found.includes("looking-back")
+                ? "brightness(100%)"
+                : "brightness(50%)",
+            }}
+            onClick={() => handleClick("looking-back")}
+          />
+          <i class="fa-regular fa-heart"></i>
+        </div>
+        <div className="lounging">
+          <img
+            src={lounging}
+            alt="Lounging"
+            style={{
+              filter: found.includes("lounging")
+                ? "brightness(100%)"
+                : "brightness(50%)",
+            }}
+            onClick={() => handleClick("lounging")}
+          />
+          <i class="fa-regular fa-heart"></i>
+        </div>
+        <div className="hiding">
+          <img
+            src={hiding}
+            alt="Hiding"
+            style={{
+              filter: found.includes("hiding")
+                ? "brightness(100%)"
+                : "brightness(50%)",
+            }}
+            onClick={() => handleClick("hiding")}
+          />
+          <i class="fa-regular fa-heart"></i>
+        </div>
+        <div className="peeking">
+          <img
+            src={peeking}
+            alt="Peeking"
+            style={{
+              filter: found.includes("peeking")
+                ? "brightness(100%)"
+                : "brightness(50%)",
+            }}
+            onClick={() => handleClick("peeking")}
+          />
+          <i class="fa-regular fa-heart"></i>
+        </div>
       </div>
     </>
   );
